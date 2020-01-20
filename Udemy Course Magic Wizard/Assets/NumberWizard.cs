8c40b27f12d6813716ca4ac0127251b31bf0ad5a
Valeska <valeskabagaric@hotmail.com>
2020-01-19 23:55:17 -0700

    Udemy Course: Added Variables and Messed with Player input
    Course by Gamedev.TV

--- Udemy Course Magic Wizard/Assets/NumberWizard.cs
+++ Udemy Course Magic Wizard/Assets/NumberWizard.cs
@@ -7,15 +7,24 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to number Wizard");
         Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is 1000");
-        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed"); 
+        }
     }
 }
