1b11aba288ce852f9b484041a6119b1ce80562b5
Valeska <valeskabagaric@hotmail.com>
2020-01-19 23:29:56 -0700

    Added New Udemy course Number Wizard by Gamedev.tv

---
+++ Udemy Course Magic Wizard/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number Wizard");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
