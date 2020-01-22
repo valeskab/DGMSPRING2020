cc2cb1fa9ef5ad8ed1462adbaed99a716e185911
Valeska <valeskabagaric@hotmail.com>
2020-01-21 23:16:43 -0700

    Udemy Course: Using if, else if & else, Scope And Context Of Variables
    Course by Gamedev.tv

--- Udemy Course Magic Wizard/Assets/NumberWizard.cs
+++ Udemy Course Magic Wizard/Assets/NumberWizard.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
+        
 
         Debug.Log("Welcome to number Wizard");
         Debug.Log("Pick a number, don't tell me what it is...");
@@ -25,10 +28,14 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
