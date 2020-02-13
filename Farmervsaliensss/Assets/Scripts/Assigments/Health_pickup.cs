ebd83081c8b752e58fd50f0ac2e195bd8d8f47b9
v <valeskabagaric@hotmail.com>
2020-02-12 17:03:56 -0700

    Class: Added Enemy and spawn manager

---
+++ Farmervsaliensss/Assets/Scripts/Assigments/Health_pickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_pickup : Pickup
+{
+    public int healthAmt = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("player health is" + healthAmt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("You Gained " + healthAmt + "Health!");
+    }
+}
