0ad8f4f0cb4ae8b5c8d8c72c9b73e38112d77cda
v <valeskabagaric@hotmail.com>
2020-02-19 17:15:55 -0700

    Class: Edited Spawn Manager

--- Farmervsaliensss/Assets/Scripts/Assigments/Enemy_move.cs
+++ Farmervsaliensss/Assets/Scripts/Assigments/Enemy_move.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Enemy_move : MonoBehaviour
 {
+    public GameObject player;
     public Transform target;
-    public float moveSpeed;
+    public int danger;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
