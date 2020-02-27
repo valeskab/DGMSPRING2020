cf48623c83964a2c6abcedb396444b84eb93b57a
v <valeskabagaric@hotmail.com>
2020-02-26 17:17:10 -0700

    Class: Added Bullet and Enemy Helath

---
+++ Farmervsaliensss/Assets/Scripts/Assigments/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   private int damage = 1;
+    public int time = 5;
+    
+    void Start()
+    {
+        //StartCoroutine(DestroyBullet());
+    }
+
+    void OnCollisionEnter(Collision other)
+
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                Debug.Log("Ouch, you hit me!");
+            }
+        }
+
+    }
+
+    int Calc(int num1, int num2)
+    {
+        int total;
+
+        total = num
+    }
+
+
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(time);
+    }
+}
---
+++ Farmervsaliensss/Assets/Scripts/Assigments/EnemyHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public int currentHealth;
+        public int maxHealth = 3;
+        public Transform spawnPoint;
+        public int points = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
