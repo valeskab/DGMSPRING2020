ddce77f278c90a56261b44f5830e93c04cff4750
Valeska <valeskabagaric@hotmail.com>
2020-03-04 01:05:50 -0700

    Created new unity project: C # practice

---
+++ C# Practice/Assets/FirstScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start function Getting called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        print("Inside OnEnable");
+
+    }
+
+    private void OnDisable()
+    {
+        print("Inside OnDisable");
+    }
+}
