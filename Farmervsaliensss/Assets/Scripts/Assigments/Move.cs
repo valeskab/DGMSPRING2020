8732991204d546bdab8344d59aa24975e6cba6e1
v <valeskabagaric@hotmail.com>
2020-03-04 17:11:46 -0700

    Class: Edited move script to add jump mechanic

--- Farmervsaliensss/Assets/Scripts/Assigments/Move.cs
+++ Farmervsaliensss/Assets/Scripts/Assigments/Move.cs
@@ -9,13 +9,18 @@ public class Move : MonoBehaviour
     public float verticalInput;
     public float horizontalInput;
 
+    public float jumpHeight;
+    public bool isGrounded;
+
+    private Rigidbody rb;
+
     public GameObject projectilePrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,6 +31,11 @@ public class Move : MonoBehaviour
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight * 1000 * Time.deltaTime);
+        }
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
